e46ba0e1d729183c4cd257829f42de5eece5b627
Minahil Sana <145493981+minahilsana@users.noreply.github.com>
2024-10-21 22:00:46 +0500

    assignment2

--- blockchain.go
+++ blockchain.go
@@ -1,51 +1,104 @@
 package assignment01bca
 
 import (
-    "crypto/sha256"
-    "encoding/hex"
-    "fmt"
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"strconv"
 )
 
-type Block struct {
-    Transaction   string
-    Nonce         int
-    PreviousHash  string
-    CurrentHash   string
+// Transaction struct to store transaction details
+type Transaction struct {
+	TransactionID           string
+	SenderBlockchainAddress string
+	RecipientBlockchainAddress string
+	Value                   float32
 }
 
-func NewBlock(transaction string, nonce int, previousHash string) *Block {
-    blk := &Block{
-        Transaction:  transaction,
-        Nonce:        nonce,
-        PreviousHash: previousHash,
-    }
-    blk.CurrentHash = CalculateHash(blk.Transaction + blk.PreviousHash + fmt.Sprint(blk.Nonce))
-    return blk
+// Block struct
+type block struct {
+	Transactions  []*Transaction
+	Nonce         int
+	PreviousHash  string
+	CurrentHash   string
 }
 
-func ListBlocks(blocks []*Block) {
-    for _, blk := range blocks {
-        fmt.Printf("Transaction: %s, Nonce: %d, PreviousHash: %s, CurrentHash: %s\n",
-            blk.Transaction, blk.Nonce, blk.PreviousHash, blk.CurrentHash)
-    }
+// Blockchain struct
+type blockChain struct {
+	Chain            []*block
+	TransactionPool  []*Transaction
+	GenesisBlock     block
+	CurrentBlock     block
 }
 
-func ChangeBlock(blk *Block, newTransaction string) {
-    blk.Transaction = newTransaction
-    blk.CurrentHash = CalculateHash(blk.Transaction + blk.PreviousHash + fmt.Sprint(blk.Nonce))
-}
-
-func VerifyChain(blocks []*Block) bool {
-    for i := 1; i < len(blocks); i++ {
-        if blocks[i].PreviousHash != blocks[i-1].CurrentHash {
-            return false
-        }
-    }
-    return true
-}
-
+// CalculateHash function to generate a hash
 func CalculateHash(stringToHash string) string {
-    hash := sha256.New()
-    hash.Write([]byte(stringToHash))
-    return hex.EncodeToString(hash.Sum(nil))
+	sum := sha256.Sum256([]byte(stringToHash))
+	return fmt.Sprintf("%x", sum)
+}
+
+// AddTransaction method for blockchain
+func (bc *blockChain) AddTransaction(sender, recipient string, value float32) {
+	t := &Transaction{
+		SenderBlockchainAddress: sender,
+		RecipientBlockchainAddress: recipient,
+		Value: value,
+		TransactionID: CalculateHash(sender + recipient + fmt.Sprintf("%f", value)),
+	}
+	bc.TransactionPool = append(bc.TransactionPool, t)
+}
+
+// Create a new block
+func NewBlock(transactions []*Transaction, nonce int, previousHash string) *block {
+	b := new(block)
+	b.Transactions = transactions
+	b.Nonce = nonce
+	b.PreviousHash = previousHash
+	b.CurrentHash = CalculateHash(fmt.Sprintf("%v", *b))
+	return b
+}
+
+// ListBlocks prints the blockchain
+func (bc *blockChain) ListBlocks() {
+	for i, block := range bc.Chain {
+		block.PrintBlock()
+	}
+}
+
+// PrintBlock prints a block in JSON format
+func (b *block) PrintBlock() {
+	transactionsJSON, err := json.Marshal(b.Transactions)
+	if err != nil {
+		fmt.Println("Error marshalling transactions:", err)
+		return
+	}
+
+	fmt.Printf("Block:\n")
+	fmt.Printf("Nonce: %d\n", b.Nonce)
+	fmt.Printf("Previous Hash: %s\n", b.PreviousHash)
+	fmt.Printf("Current Hash: %s\n", b.CurrentHash)
+	fmt.Printf("Transactions: %s\n", string(transactionsJSON))
+}
+
+// Proof of Work: Derive Nonce for the block
+func (b *block) DeriveNonce(difficulty int) {
+	target := ""
+	for i := 0; i < difficulty; i++ {
+		target += "0"
+	}
+
+	for {
+		hash := CalculateHash(fmt.Sprintf("%v", *b))
+		if hash[:difficulty] == target {
+			b.CurrentHash = hash
+			break
+		}
+		b.Nonce++
+	}
+}
+
+// Initiate a blockchain
+func InitiateBlockChain() *blockChain {
+	return &blockChain{}
 }
